8a95b9640e0e916e59279cc6e45a754bc7e7097c
jangjaepil <jaepill9@naver.com>
2022-05-05 15:33:37 +0900

    Init_done

--- docking/src/Init.cpp
+++ docking/src/Init.cpp
@@ -2,7 +2,7 @@
 #include "docking/Init.h"
 #include "docking/limit.h"
 #include "docking/desired_position.h"
-
+int i=0;
 bool X_LIMIT_UP;
 bool X_LIMIT_DOWN;
 bool Y_LIMIT_LEFT;
@@ -14,39 +14,30 @@ bool Init_callback(docking::Init::Request &req, docking::Init::Response &res)
   docking::desired_position desired_xyz;
   res.Init_done = 0;
   if(req.Init_start == 1){
-    
-    desired_xyz.request.x = 400;                  //400
-    desired_xyz.request.y = -400;                 //-400
-    desired_xyz.request.z = 0;
-    
-    
-    client->call(desired_xyz); 
-    ROS_INFO("Initializing" );
-     while(1)
-   {    
-        ROS_INFO("Initializing1" );
-   	if(desired_xyz.response.done ==1)
-   	{
-   	  desired_xyz.request.x = -100;              //-100
-          desired_xyz.request.y = 20;                // 20
-          desired_xyz.request.z = 0;
-   	  //ROS_INFO("Init done: %d",(int)res.Init_done );
-   	  client->call(desired_xyz); 
-   	  
-   	  break;
-   	}
-   }
    
-    while(1)
-    {   ROS_INFO("Initializing2" );
-        if(desired_xyz.response.done ==1)
-        { 
+    
+   
+      desired_xyz.request.x = 400;                  //400
+      desired_xyz.request.y = -400;                 //-400
+      desired_xyz.request.z = 0;
+      client->call(desired_xyz); 
+      ROS_INFO("Initializing" );
+      
+      desired_xyz.request.x = -100;              //-100
+      desired_xyz.request.y = 20;                // 20
+      desired_xyz.request.z = 0;
+      client->call(desired_xyz); 
+   	 
+       	  
+   	  
+     
+     if(desired_xyz.response.done ==1) 
+     {
+         res.Init_done =1;
+         ROS_INFO("Init done  %d ",(int)res.Init_done );
           
-          res.Init_done =1;
-          ROS_INFO("Init done  %d ",(int)res.Init_done );
-          break;
-        }
-    }
+     }   
+    
    
    
   }
@@ -78,5 +69,5 @@ int main(int argc, char **argv)
   
   ros::spin();
 
-  return 0;
+  return 1;
 }
--- docking/src/motor_controller.cpp
+++ docking/src/motor_controller.cpp
@@ -2,8 +2,8 @@
 #include "docking/xyInput.h"
 #include "docking/desired_position.h"
 #include "dynamixel_workbench_msgs/DynamixelCommand.h"
-
-
+#define limit 40
+int limit_sign = 0;
 ros::ServiceClient* client1;
 ros::ServiceClient* client2;
 
@@ -15,30 +15,74 @@ bool desired_positionCallback(docking::desired_position::Request &req, docking::
   float z = req.z; //absolute position mm
   res.done = 0;
   
+  int x_limit = int(x)/limit;
+  float x_remain = x-x_limit*limit;
+  ROS_INFO("x_limit: %d",x_limit );
+  ROS_INFO("x_remain: %f",x_remain );
+  
+  int y_limit = int(y)/limit;
+  float y_remain = y-y_limit*limit;
+  ROS_INFO("y_limit: %d",y_limit );
+  ROS_INFO("y_remain: %f",y_remain );
+  
   dynamixel_workbench_msgs::DynamixelCommand zInput;
   zInput.request.value = -(4096/360)*(360/100)*z; //resolutoin 4096  deg/mm = 360/100(wil be experienced)     
   zInput.request.id = 3;
   zInput.request.addr_name = "Goal_Position";
   zInput.response.comm_result = 0;
+  client1->call(zInput);
+  
   
   docking::xyInput xyInput;
-  xyInput.request.x = x;
-  xyInput.request.y = y;
+  xyInput.request.y = 0;
   xyInput.response.done = 0;
-  //ROS_INFO("done: %d", (int)xyInput.response.done);
+
+ if(x_limit>0) limit_sign =1;
+ else limit_sign  = -1;
+ 
+ for(int i=0;i<abs(x_limit);i++)
+  { 
+    ROS_INFO("x_iteration: %d",i );
+    ROS_INFO("x_sign: %d",limit_sign );
+    xyInput.request.x = limit*limit_sign;
+    client2->call(xyInput);
+    
+  }
+ if(x_remain != 0)
+  {
+    xyInput.request.x = x_remain;
+    client2->call(xyInput);
+    ROS_INFO("x_remain " );
+  }
+    xyInput.request.x = 0;
+    
+ if(y_limit>0) limit_sign =1;
+ else limit_sign  = -1;   
+ 
+ for(int i=0;i<abs(y_limit);i++)
+  {  
+    ROS_INFO("y_iteration: %d",i );
+    ROS_INFO("y_sign: %d",limit_sign );
+    xyInput.request.y = limit*limit_sign;
+    client2->call(xyInput);
+    
+  }
+  if(y_remain != 0) 
+  { 
+     xyInput.request.y = y_remain;
+     client2->call(xyInput);
+     ROS_INFO("y_remain " );
+  }
+  
+  
    
-   client1->call(zInput);
-   client2->call(xyInput);
-  //ROS_INFO("DONE: %d",(int)xyInput.response.done);
-   while(1)
-   {
    	if(xyInput.response.done ==1 /*&& zInput.response.comm_result ==1*/)
    	{
    	  res.done = 1;
    	  ROS_INFO("done: %d",(int)res.done );
-   	  break;
+   	  
    	}
-   }
+   
    return 1;
 }
 
